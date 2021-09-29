fd46bfa16bf602b0ccea6a23aecbdce811c53fcb
miraejang <mirae611@nate.com>
2021-09-29 14:31:16 +0900

    create header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { videosApi } from './api';
 import VideoDetail from './components/video_detail/video_detail';
 import styles from './App.module.css';
+import Header from './components/header/header';
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -22,13 +23,13 @@ function App() {
   };
 
   return (
-    <>
-      <h1>Youtube</h1>
+    <div className={styles.youtube}>
+      <Header />
       <div className={styles.content}>
         {selectedVideo && <VideoDetail selectedVideo={selectedVideo} />}
         <VideoList videos={videos} videoClick={videoClick} selectedVideo={selectedVideo ? true : false} />
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/header/header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+function Header(props) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.menuBox}>
+        <div className={styles.menuBtn}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className={styles.logo}>
+          <div className={styles.logoIcon}>
+            <FontAwesomeIcon icon={faYoutube} />
+          </div>
+          <span className={styles.logoTxt}>Youtube</span>
+        </div>
+      </div>
+      <div className={styles.searchBox}>
+        <form className={styles.form}>
+          <input className={styles.input} type="text" placeholder="Search" />
+          <button className={styles.searchBtn}>
+            <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
+          </button>
+        </form>
+      </div>
+      <div className={styles.settingBox}></div>
+    </header>
+  );
+}
+
+export default Header;
