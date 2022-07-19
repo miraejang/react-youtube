eb73fe9991f9ad8d9f268137587df39429f09d84
miraejang <mirae611@nate.com>
2022-07-19 17:18:11 +0900

    로그인 기능 추가

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faSignOutAlt,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 import Search from '../search/search';
 import { Link } from 'react-router-dom';
 
 const Header = ({
+  authService,
   searchTerm,
   searchSubmit,
   valueChange,
   clickLogo,
   clickNavBtn,
 }) => {
+  const settingsRef = useRef();
+  const [user, setUser] = useState(null);
+  const [settingsDisplay, setSettingsDisplay] = useState(false);
+
+  const login = () => {
+    authService
+      .login()
+      .then(data =>
+        setUser({ email: data.user.email, name: data.user.displayName })
+      );
+  };
+
+  const logout = () => {
+    authService.logout();
+  };
+
+  const clickAccount = () => {
+    settingsRef.current.style.display = settingsDisplay ? 'none' : 'block';
+    setSettingsDisplay(!settingsDisplay);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.menuBox}>
@@ -33,7 +59,29 @@ const Header = ({
         searchTerm={searchTerm}
         valueChange={valueChange}
       />
-      <div className={styles.settingBox}></div>
+      <div className={styles.accountBox}>
+        <div className={styles.account}>
+          {!user && (
+            <button className={styles.loginBtn} onClick={login}>
+              <FontAwesomeIcon className={styles.icon} icon={faUser} />
+              로그인
+            </button>
+          )}
+          {user && (
+            <button className={styles.accountBtn} onClick={clickAccount}>
+              {user.name}
+            </button>
+          )}
+        </div>
+        <ul ref={settingsRef} className={styles.settings}>
+          <li>
+            <button className={styles.logoutBtn} onClick={logout}>
+              <FontAwesomeIcon className={styles.icon} icon={faSignOutAlt} />
+              로그아웃
+            </button>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
